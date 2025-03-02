0a267855279fb0abf78f331ac18770a2cbc67e72
Ryan Safa <safarzadehryan@gmail.com>
2025-03-01 20:51:38 -0800

    fix persistent navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "~/styles/globals.css";
+import NavBar from "~/components/ui/NavBar";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
@@ -14,7 +15,10 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import LanguageFeed from "~/app/language-feed"
-import NavBar from "~/components/ui/NavBar";
 
 export default function HomePage() {
   return (
     <main>
-      <NavBar />
       <LanguageFeed />
     </main>
   );
