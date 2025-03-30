552737cdb3af4a1fead9d3c282b5cb556aa72ea5
Tommy Dinh <112193927+tomdkd9@users.noreply.github.com>
2025-03-29 17:18:58 -0700

    update page.tsx for LLM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import LanguageFeed from "~/app/language-feed"
+import LanguageFeed from "~/app/language-feed";
+import Chatbot from "~/components/chatbot";
 
 export default function HomePage() {
   return (
     <main>
       <LanguageFeed />
+      <Chatbot /> {}
     </main>
   );
 }
